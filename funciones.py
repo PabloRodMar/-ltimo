e02c9aeca25fcabe8cd66e676a71814fd789bf7e
Pablo Rodríguez Martín <alu0101469867@ull.edu.es>
2025-02-11 17:25:00 +0000

    Codigo

---
+++ funciones.py
@@ -0,0 +1,17 @@
+﻿def sumar(a, b):
+  return a + b
+
+def multiplicar(a, b):
+  return a * b
+
+def dividir(a, b):
+  return a / b
+
+def restar(a, b):
+  return a - b
+
+if __name__ == '__main__':
+  sumar(1,2)
+  multiplicar(2,2)
+  dividir(4,2)
+  restar(5,1)
